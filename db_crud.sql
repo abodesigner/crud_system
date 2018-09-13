c154eba987d4fe2ecafd611a2dea441ecec4be88
m_abosalem <mohammed.abosalim@gmail.com>
2018-09-13 09:19:08 +0200

    Adding all files

---
+++ db_crud.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 13, 2018 at 09:01 AM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `contact` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `active` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `members`
+--
+ALTER TABLE `members`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRUD System</title>
+
+	<!-- bootstrap -->
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
+
+	<!-- datatabels -->
+	<link rel="stylesheet" type="text/css" href="assets/datatables/dataTables.min.css">
+
+	<!-- style.css -->
+	<link rel="stylesheet" type="text/css" href="custom/css/style.css">
+
+</head>
+<body>
+
+	<div class="container">
+		<h1 class="text-center">Welcome to CRUD System</h1>
+		<div class="row">
+			<div class="col-md-12">
+				<table id="example" class="display" style="width:100%">
+			        <thead>
+			            <tr>
+			                <th>NO.</th>
+			                <th>Name</th>
+			                <th>Contacts</th>
+			                <th>Address</th>
+			                <th>Active</th>
+			                <th>Options</th>
+			            </tr>
+			        </thead>
+			        <tbody>
+			            <tr>
+			                <td>1</td>
+			                <td>Anas Mohamed</td>
+			                <td>012473269</td>
+			                <td>Galaa st</td>
+			                <td>active</td>
+			                <td>controls</td>
+			            </tr>
+			            <tr>
+			                <td>1</td>
+			                <td>Anas Mohamed</td>
+			                <td>012473269</td>
+			                <td>Galaa st</td>
+			                <td>active</td>
+			                <td>controls</td>
+			            </tr>
+			            <tr>
+			                <td>1</td>
+			                <td>Anas Mohamed</td>
+			                <td>012473269</td>
+			                <td>Galaa st</td>
+			                <td>active</td>
+			                <td>controls</td>
+			            </tr>
+			        </tbody>
+			    </table>
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+
+
+
+
+	<script type="text/javascript" src="assets/jquery/jquery.min.js"></script>
+	<script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/datatables/dataTables.min.js"></script>
+	<script type="text/javascript" src="custom/js/main.js"></script>
+
+</body>
+</html>
